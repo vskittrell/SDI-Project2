eecaaf5cdcabafcf5bf4d8908fb3d919be072e47
Vincent S Kittrell <vskittrell@fullsail.edu>
2014-05-15 00:07:43 -0400

    Update kittrell-vincent-project2.js
    I took out the initial alert. Added global variables. Added a function.

--- kittrell-vincent-project2/kittrell-vincent-project2.js
+++ kittrell-vincent-project2/kittrell-vincent-project2.js
@@ -1,8 +1,28 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
 
 
 //Vincent Kittrell
 // SDI 1405
 //Project 2
 
-//Variables
+//Global Variables
+var myQuestion = "adventure";
+var human = true;
+var myAge = 100;
+
+
+
+//functions
+var myProcedure = function(MyName) {
+    
+    console.log("What is your " + MyName);
+    
+};
+
+var theName = prompt("Enter a name: ");
+
+
+
+//Main Code
+myProcedure(theName);
+
